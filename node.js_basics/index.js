d68d79190c63f6b429c7376437f5efeb950ea147
jaseena-k <jaseenajas596@gmail.com>
2026-02-20 10:38:45 +0530

    fix

--- node.js_basics/index.js
+++ node.js_basics/index.js
@@ -1,76 +1,26 @@
-const express = require("express")
-const Joi = require("joi")
+const express =require('express')
 const app = express()
-app.use(express.json())
+const multer =require('multer')
+const path =require('path')
 
-const courses = [
-    { id: 1, name: 'mernstack' },
-    { id: 2, name: 'webdevelopment' },
-    { id: 3, name: 'datascience' }
-]
-
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
-app.get('/api/courses', (req, res) => {
-    res.send(courses)
-})
-app.post('/api/courses', (req, res) => {
-    console.log("success", req.body)
-
-   const {error} = validateCourse(req.body)
-   console.log("error  : ", error);
-   
-      if (error)
-         res.status(400).send(error.details[0].message)
-        
-    
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
+// storage configuration 
+const storage = multer.diskStorage({
+    destination : function(req,file,cb){
+        cb(null,path.join(__dirname,'uploads'))
+    },
+    filename:function(req,file,cb){
+        cb(null,Date.now()+'-'+file.originalname)
     }
-    courses.push(course)
-    res.send(course)
 })
+const upload = multer({storage})
 
-app.put('/api/courses/:id',(req,res)=>{
-    const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course) return res.status(400).send("The given  id is not found")
-   
-    const {error} = validateCourse(req.body)
-      if (error) return res.status(400).send(error.details[0].message)
-
-    course.name =req.body.name;
-    res.send(course)
-
+app.get('/',(req,res)=>{
+    res.send('Hi Jasi')
 })
-
-function validateCourse(course){
-    // console.log("validator : ",course);
-    
-    const schema ={
-        name: Joi.string().min(3).required()
-    }
-    console.log("validated");
-     
-    return Joi.validate(course,schema)
-    
-}
-
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course) return res.status(404).send("The given  id is not found")
-     res.send(course)
+app.post('/api/upload',upload.single('file'),(req,res)=>{
+    res.json({"data":req.file,"message":"success"})
 })
-
-app.delete('/api/courses/:id',(req,res)=>{
-
-const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course) return res.status(400).send("The given  id is not found")
-   const index = courses.indexOf(course)
-    courses.splice(index,1)
-    res.send(course)
+const port =process.env.PORT || 3000
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`)
 })
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`port number is ${port}..`))
